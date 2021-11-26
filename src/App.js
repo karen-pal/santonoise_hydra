2c66aa4a942c7e2ee829aa63354c47958c6a8fa7
Karen Palacio <karen.palacio.1994@gmail.com>
2021-11-25 23:04:15 -0300

    contenido y estilos

--- src/App.js
+++ src/App.js
@@ -5,9 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <div  className="App-logo" alt="logo" >
+    🧬🧬GAGTTGCCGGGA∅Karen Palacio∅GAGTTGCCGGGA🧬🧬
+      </div>
+        <p className="title">
+        Laboratorio live coding_<br />
+        Visuales con Hydra
         </p>
         <a
           className="App-link"
@@ -15,9 +18,46 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
+<link href="https://fonts.googleapis.com/css2?family=Courier+Prime&family=Roboto+Mono&display=swap" rel="stylesheet"/>
+      <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet" />
       </header>
+      <body>
+            <section>
+          <h1 className="section-title">
+          info taller
+          </h1>
+              <b>Lugar:</b> La cúpula. Humberto Primo 14, depto 7. Córdoba, Argentina<br />
+              <b>Horario:</b> de 10:30 a 14:30 hs.<br />
+              <b>No se asumirán conocimientos previos</b><br />
+              <b>Requisitos:</b> llevar compu. En lo posible, llevar zapatillas, alargadores, etc.<br /><br />
+               <b>Descripción:</b> <br /><p className="descripcion">Le livecoder de visuales puede, en vivo, a través de la programación improvisada, generar juegos semánticos con el espacio y los sonidos de un evento. Puede contrastar, reforzar o resemantizar. Puede interactuar con la arquitectura, puede dirigir la mirada, y puede transmitir información con intención. También puede proyectar lindas luces de colores. <br />
+      En este laboratorio se va a introducir a Hydra; el proyecto de Software Libre de Olivia Jack ( @o_jack ), en un formato vivencial, práctico y experimental. <br />
+¿Qué implica programar visuales? ¿Qué implica hacer una performance en vivo? ¿Cómo se generan sentidos a través de la práctica? </p><br />
+                <p>El laboratorio será un ambiente libre de violencias. Se priorizarán las voces y necesidades de femeninidades y disidencias.</p>
+
+            </section>
+            <section>
+          <h1 className="section-title">
+          apuntes de clase
+          </h1>
+      Coming soon TM
+            </section>
+            <section>
+          <h1 className="section-title">
+          links
+          </h1>
+      Coming soon TM
+            </section>
+      </body>
+            <footer>
+          <h1 className="section-title">
+         contacto 
+          </h1>
+        <h2>Karen Palacio</h2>
+      instagram.com/kardaver<br />
+      karen.palacio.1994@gmail.com<br />
+            </footer>
     </div>
   );
 }
